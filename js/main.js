40b48f6e34fd73a4dd4c5e42d18799a0dc8a9f89
Yana <yanakolomiets@gmil.com>
2020-08-20 19:34:03 +0300

    buttons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(function(){
 
-$('.photo__slider').slick();
+$('.photo__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/Arrow-left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/Arrow-right.svg" alt=""></button>'
+});
 
 });
